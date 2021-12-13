a58baff1298dfcbbc800fa9c770fac5bf3fe2c3d
Danushan <it20051648@my.sliit.lk>
2021-12-13 11:24:15 +0530

    addd deatails page

---
+++ navigator/lib/screens/details.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class DeatailPage extends StatelessWidget {
+  const DeatailPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Deatail Page"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "This Is Our Deatail Page ",
+                style: TextStyle(fontSize: 18),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Go Home'),
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12), // <-- Radius
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+}
