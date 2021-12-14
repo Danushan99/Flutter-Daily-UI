10d1341bd843baee993bc6bc0679c39f74cb2e4d
Danushan <it20051648@my.sliit.lk>
2021-12-14 19:24:27 +0530

    under text

--- pizza_app_ui/lib/pages/FirstScreen.dart
+++ pizza_app_ui/lib/pages/FirstScreen.dart
@@ -76,6 +76,9 @@ class _FirstScreenState extends State<FirstScreen> {
                 ),
                 const SizedBox(
                   height: 30,
+                ),
+                Align(
+                  child: Text(""),
                 )
               ],
             ),
