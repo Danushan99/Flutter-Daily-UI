f42a3eccb58e4bbcadd5bc16e95f663c09585516
Danushan <it20051648@my.sliit.lk>
2021-12-16 00:25:24 +0530

    set animations

--- pizza_app_ui/lib/animations/fadeanimation.dart
+++ pizza_app_ui/lib/animations/fadeanimation.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: import_of_legacy_library_into_null_safe
+
 import 'package:flutter/material.dart';
 import 'package:simple_animations/simple_animations.dart';
 
@@ -10,11 +12,24 @@ class FadeAnimation extends StatelessWidget {
   Widget build(BuildContext context) {
     final tween = MultiTrackTween([
       Track('opacity')
-          .add(const Duration(milliseconds: 500), Tween(begin: 0.0, end: 1.0)),
+          .add(Duration(milliseconds: 500), Tween(begin: 0.0, end: 1.0)),
       Track('translateX').add(
-          const Duration(milliseconds: 500), Tween(begin: 120.0, end: 0.0),
+          Duration(milliseconds: 500), Tween(begin: 120.0, end: 0.0),
           curve: Curves.easeOut),
     ]);
-    return Container();
+    return ControlledAnimation(
+      delay: Duration(
+        milliseconds: (500 * delay).round(),
+      ),
+      tween: tween,
+      child: child,
+      builderWithChild: (context, child, animation) => Opacity(
+        opacity: animation['opacity'],
+        child: Transform.translate(
+          offset: Offset(animation['translateX'], 0),
+          child: child,
+        ),
+      ),
+    );
   }
 }
