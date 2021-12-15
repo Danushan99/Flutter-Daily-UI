47225b670eb22905be08feca0956c93cb65489d9
Danushan <it20051648@my.sliit.lk>
2021-12-15 21:46:27 +0530

    set time

--- pizza_app_ui/lib/animations/fadeanimation.dart
+++ pizza_app_ui/lib/animations/fadeanimation.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_animations/simple_animations.dart';
 
 class FadeAnimation extends StatelessWidget {
   late final double delay;
@@ -7,6 +8,13 @@ class FadeAnimation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final tween = MultiTrackTween([
+      Track('opacity')
+          .add(const Duration(milliseconds: 500), Tween(begin: 0.0, end: 1.0)),
+      Track('translateX').add(
+          const Duration(milliseconds: 500), Tween(begin: 120.0, end: 0.0),
+          curve: Curves.easeOut),
+    ]);
     return Container();
   }
 }
